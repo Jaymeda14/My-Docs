f6c23feee2cc7dbb8e54157aedd865254383dac9
JaimeFreire <115376166+Jaymeda14@users.noreply.github.com>
2024-02-07 12:48:28 -0300

    atualizei

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 function gerarCurriculo() {
-    // Obtenha os dados do formulário
-    var nome = document.getElementById('nome').value;
-    var email = document.getElementById('email').value;
+    // Obtenha os dados do vendedor
+    var nomeVendedor = document.getElementById('nomeVendedor').value;
+    var cpfVendedor = document.getElementById('cpfVendedor').value;
+    var rgVendedor = document.getElementById('rgVendedor').value;
+
+    // Obtenha os dados do comprador
+    var nomeComprador = document.getElementById('nomeComprador').value;
+    var cpfComprador = document.getElementById('cpfComprador').value;
+    var rgComprador = document.getElementById('rgComprador').value;
     // ... obter outros dados
 
-    // Crie o currículo formatado
-    var curriculo = `
-        <h2>${nome}</h2>
-        <p>Email: ${email}</p>
+    
+
+    // Crie o recíbo formatado
+    var recibo = `
+                Pelo Presente Instrumento Particular de CONTRATO DE COMPRA E VENDA DE IMÓVEL. Eu, ${nomeVendedor}, brasileiro,  portador do RG nº ${rgVendedor} e do CPF: ${cpfVendedor}, declaro ter recebido de ${nomeComprador}, brasileira, portadora do RG nº ${rgComprador} PC/PA e do CPF: ${cpfComprador}, a quantia de R$ 11.000,00 (onze mil reais)
+
+        
         <!-- Adicione outras informações do currículo -->
     `;
 
     // Atualize a seção do currículo gerado na página
-    document.getElementById('curriculoGerado').innerHTML = curriculo;
+    document.getElementById('reciboGerado').innerHTML = recibo;
 }
